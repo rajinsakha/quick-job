89f5c5e70ac768d05dba8d102722ee8479b7070a
Rajin Sakha <rajinsakha07@gmail.com>
2025-01-25 13:27:29 +0545

    fixed-logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center space-x-2">
-      <span className="text-2xl font-extrabold text-primary">Quick</span>
-      <span className="text-2xl font-extrabold text-gray-800">Job</span>
+    <div className="flex items-center flex-col -space-y-2">
+      <span className="text-xl font-extrabold text-primary tracking-wide">WorkSpace</span>
+      <div className="flex items-center gap-1">
+        <span className="w-8 h-[1px] bg-primary"></span>
+      <span className="text-xl font-extrabold text-primary">Nepal</span>
+      <span className="w-8 h-[1px] bg-primary"></span>
+      </div>
+      
     </div>
   );
 };
